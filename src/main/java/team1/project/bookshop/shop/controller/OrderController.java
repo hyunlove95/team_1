a1899885ed7c2405e6997a2797d020ddd255196a
hyunsung <hyunlove7980@gmail.com>
2023-03-31 08:55:13 +0900

    'hyun4'

--- src/main/java/team1/project/bookshop/shop/controller/OrderController.java
+++ src/main/java/team1/project/bookshop/shop/controller/OrderController.java
@@ -12,8 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import team1.project.bookshop.model.orders.OrdersService;
-
-
+import team1.project.bookshop.model.ship_condition.Ship_conditionService;
 
 @Controller
 public class OrderController {
@@ -22,12 +21,24 @@ public class OrderController {
 	@Autowired
 	private OrdersService ordersService;
 	
+	@Autowired
+	private Ship_conditionService ship_conditionService;
+	
 	@GetMapping("/order/list")
 	public ModelAndView getOrderList(HttpServletRequest request) {
 		List orderList = ordersService.selectAll();
 		
 		ModelAndView mav = new ModelAndView("admin/order/orderList");
 		mav.addObject("orderList", orderList);
+		return mav;
+	}
+	
+	@GetMapping("/order_detail")
+	public ModelAndView getDetail() {
+		List ship_conditionList = ship_conditionService.selectAll();
+		
+		ModelAndView mav = new ModelAndView("admin/order/orderDetail");
+		mav.addObject("ship_conditionList", ship_conditionList);
 		return mav;
 	}
 	
